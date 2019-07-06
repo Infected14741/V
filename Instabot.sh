ee5b6b65376511095d3174831a4fbf6a05f4ba52
Infected_AOFSFOF <42504662+Infected14741@users.noreply.github.com>
2019-07-06 07:08:04 +0700

    Add files via upload

---
+++ Instabot.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#version 3.0
+#HAK CIPTA MILIK INFECTED_AOFSFOF
+
+pkg update
+pkg upgrade
+pkg install git
+pkg install php
+pkg install wget
+pkg install figlet
+pkg install toilet
+pkg install python2
+pkg install python
+clear
+python2 Security.py
+python Restart.py
+clear
+git clone https://github.com/Infected14741/modules
+clear
+cd modules
+mv node_modules.zip /$HOME/Instabot-Infected-aofsfof
+cd
+cd Instabot-Infected-aofsfof
+unzip node_modules.zip
+clear
+rm -rf node_modules.zip
+git clone https://github.com/Infected14741/index
+clear
+cd index
+mv index.js /$HOME/Instabot-Infected-aofsfof
+cd
+cd Instabot-Infected-aofsfof
+rm -rf index
+git clone https://github.com/Infected14741/run
+clear
+cd run
+mv run.sh /$HOME/Instabot-Infected-aofsfof
+cd
+cd Instabot-Infected-aofsfof
+rm -rf run
+sh run.sh
+rm -rf run.sh
+rm -rf index.js
+cd
+rm - rf Instabot-Infected-aofsfof
